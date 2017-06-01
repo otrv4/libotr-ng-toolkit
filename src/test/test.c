328dacc4b4793da8d46ec194926032e42b48ba8b
Ivan Pazmino <iapazmino@gmail.com>
2017-06-01 14:44:14 -0500

    Adds make tast to style code. 'make code-style'

--- src/test/test.c
+++ src/test/test.c
@@ -3,11 +3,9 @@
 
 #include "test_otrv4_parse.c"
 
-int main(int argc, char **argv)
-{
-	g_test_init(&argc, &argv, NULL);
-	g_test_add_func("/toolkit/parse", otrv4_toolkit_test_parse_data_message);
+int main(int argc, char **argv) {
+  g_test_init(&argc, &argv, NULL);
+  g_test_add_func("/toolkit/parse", otrv4_toolkit_test_parse_data_message);
 
-	return g_test_run();
+  return g_test_run();
 }
-
