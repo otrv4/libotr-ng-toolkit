fe8dbbeae3cf607a88fdb179204b5c98249ffcf8
Denis Costa <deniscostadsc@gmail.com>
2018-07-11 16:52:20 -0300

    Remove unused code

--- src/test/test_helpers.h
+++ src/test/test_helpers.h
@@ -1,27 +1,7 @@
-//#include <decaf.h>
-//#include <decaf/ed448.h>
-//#include <goldilocks/point_448.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "../debug.h"
-
-// this is not used
-//#define otrv4_assert_cmpmem(s1, s2, len)                                       \
-  //do {                                                                         \
-    //char *__s1 = _otrng_memdump((const uint8_t *)s1, len);                     \
-    //char *__s1 = _otrng_memdump((const uint8_t *)s2, len);                     \
-    //char *__msg = g_strdup_printf(                                             \
-        //"assertion failed: (%s)\nEXPECTED (%p): %s\nACTUAL (%p): %s\n",        \
-        //#s1 " ==  " #s2, s1, __s1, s2, __s2);                                  \
-    //if (memcmp(s1, s2, len) == 0)                                              \
-      //;                                                                        \
-    //else                                                                       \
-      //g_assertion_message(G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, __msg); \
-    //free(__s1);                                                                \
-    //free(__s2);                                                                \
-    //g_free(__msg);                                                             \
-  //} while (0)
 
 #define otrv4_assert(expr)                                                     \
   do {                                                                         \
@@ -38,9 +18,3 @@ static inline void otrv4_assert_uint8_equals(const uint8_t *expected,
     g_assert_cmpint(expected[i], ==, actual[i]);
   }
 }
-
-// this is not used
-// static inline void otrv4_assert_point_equals(const ec_point_p expected,
-// const ec_point_p actual) {
-// g_assert_cmpint(decaf_448_point_eq(expected, actual), !=, 0);
-//}
