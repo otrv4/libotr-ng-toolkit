e26af30bb65dbdfa2a34cc04c78ba3be969f5c08
Anna Cruz <anna@autonomia.digital>
2018-07-25 19:57:46 -0300

    Parse query messages
    Co-authored-by: Denis Costa <deniscostadsc@gmail.com>

--- src/otrng_parse.c
+++ src/otrng_parse.c
@@ -7,13 +7,25 @@ int main(int argc, char **argv) {
   char *original_msg = NULL;
   original_msg = readotr(stdin);
 
-  data_message_s *data_msg = otrng_data_message_new();
-  otrng_header_s *header_msg = malloc(sizeof(otrng_header_s));
+  int message_type = otrng_get_message_type(original_msg);
+  int result = 1;
+ 
+  if(message_type == MSG_QUERY_STRING){
+    printf("QUERY STRING: ");
+    for(int i = 0; i < strlen(original_msg); i++){
+      printf("%c", original_msg[i]);
+    }
+  } else if (message_type == MSG_OTR_ENCODED) {
+    data_message_s *data_msg = otrng_data_message_new();
+    otrng_header_s *header_msg = malloc(sizeof(otrng_header_s));
 
-  int result = parse_data_message(data_msg, header_msg, original_msg);
-  print_data_message(header_msg, data_msg);
+    result = parse_data_message(data_msg, header_msg, original_msg);
+    print_data_message(header_msg, data_msg);
+
+    free(header_msg);
+    otrng_data_message_free(data_msg);
+  }
+
   
-  free(header_msg);
-  otrng_data_message_free(data_msg);
   return result;
 }
