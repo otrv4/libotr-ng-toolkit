9e14fe38b4717d71560e99dc74bea9b877f14f7d
Anna Cruz <anna@autonomia.digital>
2018-07-25 19:56:08 -0300

    Add readotr files
    Co-authored-by: Denis Costa <deniscostadsc@gmail.com>

---
+++ src/readotr.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char *data;
+    size_t len;
+    size_t alloclen;
+} Buffer;
+
+static void buf_new(Buffer *bufp)
+{
+    bufp->data = NULL;
+    bufp->len = 0;
+    bufp->alloclen = 0;
+}
+
+static void buf_put(Buffer *bufp, const char *str, size_t len)
+{
+    while (bufp->len + len + 1 > bufp->alloclen) {
+	char *newdata = realloc(bufp->data, bufp->alloclen + 1024);
+	if (!newdata) {
+	    fprintf(stderr, "Out of memory!\n");
+	    exit(1);
+	}
+	bufp->data = newdata;
+	bufp->alloclen += 1024;
+    }
+    memmove(bufp->data + bufp->len, str, len);
+    bufp->len += len;
+    bufp->data[bufp->len] = '\0';
+}
+
+static void buf_putc(Buffer *bufp, char c)
+{
+    buf_put(bufp, &c, 1);
+}
+
+char *readotr(FILE *stream)
+{
+    int seen = 0;
+    const char header[] = "?OTR";  
+    int headerlen = strlen(header);
+    Buffer buf;
+
+    while(seen < headerlen) {
+	int c = fgetc(stream);
+	if (c == EOF) return NULL;
+	else if (c == header[seen]) seen++;
+	else if (c == header[0]) seen = 1;
+	else seen = 0;
+    }
+
+    buf_new(&buf);
+    buf_put(&buf, header, headerlen);
+
+    /* Look for the trailing '.' */
+    while(1) {
+	int c = fgetc(stream);
+	if (c == EOF) break;
+	buf_putc(&buf, c);
+	if (c == '.') break;
+    }
+
+    return buf.data;
+}
+
---
+++ src/readotr.h
@@ -0,0 +1,7 @@
+#ifndef __READOTR_H__
+#define __READOTR_H__
+
+char *readotr(FILE *stream);
+
+#endif
+
