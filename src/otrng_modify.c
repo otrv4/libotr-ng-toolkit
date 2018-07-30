27bd893aee89fa859054fbc77b75a274c9544cb1
Anna Cruz <anna@autonomia.digital>
2018-07-30 16:16:01 -0300

    Decode and verify the data msg, also make the necessary xor

--- src/otrng_modify.c
+++ src/otrng_modify.c
@@ -88,6 +88,40 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  otrng_header_s *header_msg = malloc(sizeof(otrng_header_s));
+  if (!header_msg) {
+    return 1;
   }
+
+  result = decode_header(header_msg, original_msg);
+
+  if (result != 0) {
+    puts("Error on decode header of message");
+    return 1;
+  }
+
+  if (header_msg->type != DATA_MSG_TYPE) {
+    puts("Message is not a data message");
+    return 1;
+  }
+
+  data_message_s *data_msg = otrng_data_message_new();
+  result = decode_data_message(data_msg, original_msg);
+
+  if (result != 0) {
+    puts("Error decoding data message");
+    return 1;
+  }
+
+  for (int i = 0; i < strlen(old_msg_txt) && offset + 1 < data_msg->enc_msg_len;
+       i++) {
+    data_msg->enc_msg[offset + i] ^= (old_msg_txt[i] ^ new_msg_txt[i]);
+  }
+
+  // - Recalculate the MAC tag with the revealed MAC key associated with this
+  //   message. The new tag is attached to the data message, replacing the old
+  //   value.
+
+  otrng_data_message_free(data_msg);
   return result;
 }
