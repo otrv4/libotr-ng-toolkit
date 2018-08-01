b60c46eaee2dd11d14db97ca4a218cfd8da58ade
Anna Cruz <anna@autonomia.digital>
2018-08-01 18:08:34 -0300

    Fix style
    Co-authored-by: Denis Costa <deniscostadsc@gmail.com>

--- src/otrng_parse.c
+++ src/otrng_parse.c
@@ -16,14 +16,18 @@ int main(int argc, char **argv) {
   if (message_type == MSG_PLAINTEXT) {
     printf("PLAIN TEXT: ");
     print_string(original_msg, strlen(original_msg));
+
   } else if (message_type == MSG_TAGGED_PLAINTEXT) {
     printf("PLAIN TEXT WITH WHITESPACE TAGS: ");
     print_plaintext_formated(original_msg, strlen(original_msg));
+
   } else if (message_type == MSG_QUERY_STRING) {
     printf("QUERY STRING: ");
     print_string(original_msg, strlen(original_msg));
+
   } else if (message_type == MSG_OTR_ENCODED) {
     otrng_header_s *header_msg = malloc(sizeof(otrng_header_s));
+
     if (!header_msg) {
       return 1;
     }
@@ -34,19 +38,23 @@ int main(int argc, char **argv) {
       dake_identity_message_p identity_msg;
       result = decode_identity_message(identity_msg, original_msg);
       print_identity_message(header_msg, identity_msg);
+
     } else if (header_msg->type == AUTH_R_MSG_TYPE) {
       dake_auth_r_p auth_r_msg;
       result = decode_auth_r_message(auth_r_msg, original_msg);
       print_auth_r(header_msg, auth_r_msg);
+
     } else if (header_msg->type == AUTH_I_MSG_TYPE) {
       dake_auth_i_p auth_i_msg;
       result = decode_auth_i_message(auth_i_msg, original_msg);
       print_auth_i(header_msg, auth_i_msg);
+
     } else if (header_msg->type == DATA_MSG_TYPE) {
       data_message_s *data_msg = otrng_data_message_new();
       result = decode_data_message(data_msg, original_msg);
       print_data_message(header_msg, data_msg);
       otrng_data_message_free(data_msg);
+
     } else {
       printf("Error while trying to parse encoded message\n");
       return 1;
@@ -56,6 +64,7 @@ int main(int argc, char **argv) {
   } else if (message_type == MSG_OTR_ERROR) {
     printf("OTR ERROR: ");
     print_string(original_msg, strlen(original_msg));
+
   } else {
     printf("Error: not valid OTRv4 message!\n");
     return 1;
