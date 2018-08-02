29c09236db37e419e1f0cd6adae9c7ac70c13425
Anna Cruz <anna@autonomia.digital>
2018-08-02 18:32:30 -0300

    Decrypt data msg
    Co-authored-by: Denis Costa <deniscostadsc@gmail.com>

--- src/otrng_readforge.c
+++ src/otrng_readforge.c
@@ -8,19 +8,27 @@
     the given chain key.
   - If a new message is given, replace the message with that one, encrypt it
     and create its mac accordingly. */
-#include <libotr-ng/data_message.h>
-#include <libotr-ng/key_management.h>
-#include <libotr-ng/shake.h>
 #include <stdio.h>
 #include <string.h>
 
+#include <libotr-ng/data_message.h>
+#include <libotr-ng/key_management.h>
+#include <libotr-ng/shake.h>
+#include <libotr-ng/random.h>
+
 static uint8_t usage_message_key = 0x16;
 
-int decrypt_data_msg(const msg_enc_key_p enc_key, const data_message_s *msg) {
-  uint8_t *plain = malloc(msg->enc_msg_len);
-  if (!plain) {
+int decrypt_data_message(uint8_t *plain, const msg_enc_key_p enc_key,
+                         const data_message_s *msg) {
+
+  int err = crypto_stream_xor(plain, msg->enc_msg, msg->enc_msg_len, msg->nonce,
+                              enc_key);
+
+  if (err) {
     return 1;
   }
+  return 0;
+}
 
   int err = crypto_stream_xor(plain, msg->enc_msg, msg->enc_msg_len, msg->nonce,
                               enc_key);
@@ -32,9 +40,9 @@ int decrypt_data_msg(const msg_enc_key_p enc_key, const data_message_s *msg) {
   return 0;
 }
 
-void argv_to_buf(uint8_t **dst, size_t *written, char *arg) {
+void arg_to_buf(uint8_t **dst, size_t *written, char *arg) {
   uint8_t *buf;
-  *dst = NULL;
+  *dst = 0;
   *written = 0;
   size_t size = strlen(arg);
 
@@ -72,22 +80,36 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *chain_key = argv[2];
-  char *original_msg = argv[3];
-  // char *new_txt_msg = argv[4];
-  size_t len;
-  uint8_t *new_chain_key;
-  argv_to_buf(&new_chain_key, &len, argv[2]);
+  size_t len_chain_key;
+  uint8_t *chain_key;
+  char *original_msg = argv[2];
 
-  if (len != CHAIN_KEY_BYTES) {
+  arg_to_buf(&chain_key, &len_chain_key, argv[1]);
+
+  if (len_chain_key != CHAIN_KEY_BYTES) {
     puts("Chain key with the wrong size");
     return 1;
   }
 
   msg_enc_key_p enc_key;
   memset(enc_key, 0, sizeof(enc_key));
-  shake_256_kdf1(enc_key, sizeof(msg_enc_key_p), usage_message_key,
-                 new_chain_key, CHAIN_KEY_BYTES);
 
+  shake_256_kdf1(enc_key, sizeof(msg_enc_key_p), 0x16, chain_key, 64);
+
+  data_message_s *data_msg = otrng_data_message_new();
+  if (decode_data_message(data_msg, original_msg)) {
+    puts("Error decoding message");
+    return 1;
+  }
+
+  uint8_t *plain = malloc(data_msg->enc_msg_len);
+  if (!plain) {
+    return 1;
+  }
+
+  decrypt_data_message(plain, enc_key, data_msg);
+
+  printf("Decrypted message: %s\n", plain);
+  free(plain);
   return 0;
 }
