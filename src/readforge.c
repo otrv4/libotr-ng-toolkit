737afa1002ec332cf87ba8324933181e12c11b18
Anna Cruz <anna@autonomia.digital>
2018-08-10 14:13:42 -0300

    Refact read and forge
    Co-authored-by: Denis Costa <deniscostadsc@gmail.com>

--- src/readforge.c
+++ src/readforge.c
@@ -8,15 +8,13 @@
 #include "decode.h"
 #include "helper.h"
 
-int readforge(uint8_t **plain_text, char **encoded_data_msg,
-              char *raw_ratchet_key, char *raw_msg, uint8_t *new_txt_msg) {
+int read(uint8_t **plain_text, msg_enc_key_p enc_key, char *raw_ratchet_key, char *raw_msg, data_message_s *data_msg){
   size_t len_given_key;
   uint8_t *given_key;
   argv_to_buf(&given_key, &len_given_key, raw_ratchet_key);
 
-  msg_enc_key_p enc_key;
   if (len_given_key == CHAIN_KEY_BYTES) {
-    memset(enc_key, 0, sizeof(enc_key));
+    memset(enc_key, 0, sizeof(msg_enc_key_p));
     shake_256_kdf1(enc_key, sizeof(msg_enc_key_p), usage_message_key, given_key,
                    64);
   } else if (len_given_key == ENC_KEY_BYTES) {
@@ -26,18 +24,19 @@ int readforge(uint8_t **plain_text, char **encoded_data_msg,
     return 1;
   }
 
-  data_message_s *data_msg = otrng_data_message_new();
-  if (decode_data_message(data_msg, raw_msg)) {
-    fprintf(stderr, "Error decoding message");
-    return 1;
-  }
-
+  //L = M + N (M plain, N TLVS)
   *plain_text = malloc(data_msg->enc_msg_len);
   if (!plain_text) {
+    fprintf(stderr, "Out of memory");
     return 1;
   }
 
   decrypt_data_message(*plain_text, enc_key, data_msg);
+  return 0;
+}
+
+int forge(char **encoded_data_msg, uint8_t *new_txt_msg, data_message_s *data_msg,
+          msg_enc_key_p enc_key){
 
   if (new_txt_msg != NULL) {
     encrypt_data_message(data_msg, new_txt_msg, sizeof(new_txt_msg), enc_key);
@@ -53,7 +52,35 @@ int readforge(uint8_t **plain_text, char **encoded_data_msg,
 
     serialize_and_remac(encoded_data_msg, data_msg, data_msg->mac);
   }
-  otrng_data_message_free(data_msg);
+  return 0;
+}
 
+int readforge(uint8_t **plain_text, char **encoded_data_msg,
+              char *raw_ratchet_key, char *raw_msg, uint8_t *new_txt_msg) {
+
+  data_message_s *data_msg = otrng_data_message_new();
+  if (decode_data_message(data_msg, raw_msg)) {
+    fprintf(stderr, "Error decoding message");
+    return 1;
+  }
+
+  msg_enc_key_p enc_key;
+
+  if(read(plain_text, enc_key, raw_ratchet_key, raw_msg, data_msg)){
+    fprintf(stderr, "Error reading message");
+    return 1;
+  }
+
+  printf("tamanho é %ld\n", sizeof(new_txt_msg));
+  printf("tamanho é %ld\n", sizeof(uint8_t*));
+
+  //1) Extract TLVs from plain_text to add them back into the new encrypted msg
+
+  if(forge(encoded_data_msg, new_txt_msg, data_msg, enc_key)){
+    fprintf(stderr, "Error forging message");
+    return 1;
+  }
+
+  otrng_data_message_free(data_msg);
   return 0;
 }
